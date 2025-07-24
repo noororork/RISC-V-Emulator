327f2f5033b53d9a477fe3a2f160757bacb370d3
Noor Khalil <noorhkhalil@gmail.com>
2025-07-24 16:00:12 +0100

    merging emulator and cpu files

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,13 +1,33 @@
 // cpu.cpp
 // Holds registers and the pc and simulates Fetch Execute
 
-#include "../include/emulator.h"
-#include <vector>
+#include "../include/memory.h"
+#include "../include/parser.h"
 #include <cstdint>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>  // File stream library
 using namespace std;
 
 class CPU{
     private:
         uint32_t regs[32];
-        uint32_t pc;   
+        uint32_t pc;
+        Memory memory;
+        Parser parser;
+    public:
+        int loadProgram(string fileName){
+            ifstream f(fileName);
+
+            if(!f.is_open()){
+                cerr << "Error opening file";
+            }
+            string s;
+            while (getline(f,s)){
+                parser.parse(s);
+            }
+        }
+        
+};   
 };
---
+++ src/parser.cpp
@@ -0,0 +1,14 @@
+// parser.cpp
+// Parses lines that have been passed through and sends them to execute
+
+#include <cstdint>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Parser{
+    public:
+        void parse(string s){
+            s.trim();
+        }
+};
