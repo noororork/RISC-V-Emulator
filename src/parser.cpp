5b8f77f0aa12fc97bedf98c8c5a3fb28ab448ce3
Noor Khalil <noorhkhalil@gmail.com>
2025-07-26 22:19:48 +0100

    more parsing

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,18 +6,32 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <map>
+#include <string>
 #include <cctype> // For isspace
 using namespace std;
 
 class Parser{
+    private:
+        map<string, char> encodingTypes = {{"lw", 'i'}, {"sw", 's'}};
     public:
         void parse(string s){
             s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end()); // Removes whitespace
             
             istringstream iss(s);
-            string op, arg1, arg2;
-            iss >> op >> arg1 >> arg2; // Seperating line into address and operation
+            string op;
+            iss >> op; // Seperating operation
+
+            char type = encodingTypes.at(op);
+            seperate(s, type);
             
-            arg1.erase(remove_if(arg1.begin(), arg1.end(), ','), arg1.end()); // Removes comma after first address
+            //arg1.erase(remove_if(arg1.begin(), arg1.end(), ','), arg1.end()); // Removes comma after first address 
+        }
+        void seperate(string s, char type){
+            istringstream iss(s);
+            switch (type){
+                case 'i': 
+                    string op, arg
+            }
         }
 };
