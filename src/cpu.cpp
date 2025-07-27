ee00c75f24570cf5ac19cc347646357626a8824d
Noor Khalil <noorhkhalil@gmail.com>
2025-07-27 19:44:54 +0100

    moved file

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -19,6 +19,10 @@ int CPU::loadProgram(string fileName){ // CPU:: = Scope resolution operator- sho
     // Finding the size of the file
     long size;
     ifstream binary_file(fileName, ios::binary | ios::in);
+    if (!binary_file.is_open()) {
+        cerr << "Failed to open file: " << fileName << endl;
+        return 1;
+    }
     binary_file.seekg(0, ios::end); // Seekg allows us to find arbitrary position in file- in this case the end
     size = binary_file.tellg();
     binary_file.seekg(0, ios::beg); // 0 is offset
