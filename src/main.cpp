88ba046482e6382c443c451eb0ba2094a8a35267
Noor Khalil <noorhkhalil@gmail.com>
2025-07-27 19:58:58 +0100

    made binary file for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include <ios>
+#include <fstream>
 #include "../include/cpu.h"
 
 int main(){
+    // This array represents raw bytes: 0x33, 0x0D, 0xFF, 0x00
+    uint8_t rawData[] = { 0x33, 0x0D, 0xFF, 0x00 };
+
+    // Open a file in binary write mode
+    std::ofstream outFile("program.bin", std::ios::binary);
+
+    // Write the raw bytes into the file
+    outFile.write(reinterpret_cast<char*>(rawData), sizeof(rawData));
+
+    outFile.close();
     CPU cpu;
-    cpu.loadProgram("file.bin");
+    cpu.loadProgram("program.bin");
+    return 0;
 }
